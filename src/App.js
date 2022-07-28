adc86f8973982075a348444318198e0b6cd77103
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2022-07-29 09:25:40 +1200

    Added navigation bar, profile section and about section.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Projects</li>
+          </ul>
+        </nav>
       </header>
+      <div class="App-main">
+        <section id="profile">
+          <img src="/icon.png" height="200"/>
+          <h1>Bond Trinh</h1><br/>
+          <small id="profile-subtitle">Student at Auckland
+          University of Technology / Aspiring Software
+          Developer</small>
+        </section>
+        <section id="about">
+          <h2 class="about-subtitle">About</h2>
+            <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet fuga, deserunt ipsa impedit cum sequi beatae non pariatur corporis recusandae nemo perspiciatis nostrum. Cupiditate fugiat quae natus voluptatibus. Architecto, est.</span><span>Explicabo hic asperiores laboriosam rerum. Ipsam, nesciunt maxime iste impedit ratione quidem ipsa et animi reprehenderit minus placeat consectetur ab neque ex similique quas dignissimos non eum dolore eveniet rem!</span><span>Ducimus, tempora! Labore repudiandae doloremque dolor itaque dicta tempora, molestiae incidunt repellendus, voluptate nam veritatis asperiores quisquam consequatur nulla. Excepturi, nesciunt expedita! Eius, ullam quis quidem neque tenetur dolore labore.</span><span>Doloribus necessitatibus voluptatem laudantium veniam soluta voluptatum totam? Adipisci error amet accusantium a! Nihil delectus commodi placeat sint omnis id exercitationem blanditiis, ullam rem ipsa veniam ad recusandae. Sint, quod.</span><span>Velit, facere ut et fuga quae quis laborum quisquam ipsam saepe veniam cum molestiae unde placeat doloribus neque? Fuga aperiam doloremque earum, dolore ducimus consequatur modi atque totam nostrum! Amet?</span><span>Accusamus quasi odio velit repellat veniam, amet earum necessitatibus cupiditate quaerat mollitia suscipit dicta possimus nulla, fuga ab esse aspernatur distinctio minus? Nesciunt, quisquam! Nihil tempora dolorem corporis accusamus quidem?</span><span>Fugit vitae accusamus impedit similique, natus tenetur laborum et eveniet dignissimos esse distinctio dolorem sed aliquid fuga quod id perspiciatis corporis, ea, quaerat nobis iusto. Distinctio neque laborum explicabo unde!</span><span>Doloremque nobis aliquid ullam dolor? Aut ullam omnis, ut maxime harum praesentium recusandae labore repellat, dolorum aliquam beatae distinctio. Corporis reiciendis cumque iste minima suscipit perspiciatis ipsa cum quisquam qui.</span><span>Necessitatibus, commodi repellat veniam aliquam odit fugit illum provident laudantium pariatur dolore dolores a, error veritatis obcaecati accusamus sed ut debitis corporis at rerum consequuntur doloremque id. Aspernatur, velit facilis!</span><span>Possimus alias aliquid vero laudantium delectus nisi quia, velit et distinctio, autem veritatis at quos. Totam, nihil voluptatibus. Libero nesciunt illo, fugit quibusdam alias totam velit ex quo sed repudiandae?</span></p>
+          
+        </section>
+      </div>
     </div>
   );
 }
